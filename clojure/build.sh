022354dd7bf3eadcac3819d1bf104a71d4699746
Christian Johansen <christian.johansen@mattilsynet.no>
2024-11-08 11:02:43 +0100

    feat: add first-class Clojure distribution

---
+++ clojure/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -fr resources
+cd ../
+npm install
+npm run build
+cd clojure
+clojure -A:dev -T:build build-css-modules
+clojure -A:dev -T:build export-all-svgs
+clojure -A:dev -T:build export-css
