8dd0b422237565b9ccbe100ea6e3f307e79b65ae
Christian Johansen <christian.johansen@mattilsynet.no>
2024-11-12 13:52:36 +0100

    Bare kjør vite-byttet dersom det trengs

--- clojure/build.sh
+++ clojure/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e
+
 rm -fr resources
-cd ../
-npm install
-npm run build
-cd clojure
+
+if test -d dist; then
+    $(cd ../ && npm install && npm run build)
+fi
+
 clojure -A:dev -T:build build-css-modules
 clojure -A:dev -T:build export-all-svgs
 clojure -A:dev -T:build export-css
