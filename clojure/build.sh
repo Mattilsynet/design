c857cd3d093a0298ff12de35f7e5cba6975de446
eirikbacker <eirik.backer@mattilsynet.no>
2025-01-07 12:28:28 +0100

    chore: release Clojure library 0.1.0.11

--- clojure/build.sh
+++ clojure/build.sh
@@ -4,10 +4,6 @@ set -xe
 
 rm -fr resources
 
-if test -d mtds; then
-    $(cd ../ && npm install && npm run build)
-fi
-
 clojure -A:dev -T:build build-css-modules
 clojure -A:dev -T:build export-all-svgs
 clojure -A:dev -T:build export-css
