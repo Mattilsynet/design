80b7925897abd6fc52359e70698daa481ae499bd
eirikbacker <eirik.backer@mattilsynet.no>
2025-01-07 12:32:10 +0100

    chore: cleanup

--- designsystem/modal/modal.stories.tsx
+++ designsystem/modal/modal.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles.module.css";
 
