e12de95882dec37a8002007aefaa3e9a2b2ff721
Marius Sørensen Dreyer <mariussd@users.noreply.github.com>
2025-01-08 16:55:14 +0100

    feat/add search to illustrations (#77)
    * feat: move icons, categorize them and make them searchable
    
    * docs: use u-datalist

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,48 +3,53 @@ import path from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 import fg from "fast-glob";
 import { version } from "../package.json";
+import illustrations from "../public/illustrations/illustrations.json";
 
-const PUBLIC_DIR = path.resolve('./public');
-const FOLDERS = '@(identitet|designsystem|profilering)';
-const IGNORE = '!(avatar|card|chip)'
+const PUBLIC_DIR = path.resolve("./public");
+const FOLDERS = "@(identitet|designsystem|profilering)";
+const IGNORE = "!(avatar|card|chip)";
 
 export default {
-  stories: [
-    `../${FOLDERS}/**/${IGNORE}.mdx`,
-    `../${FOLDERS}/**/${IGNORE}.stories.@(js|jsx|mjs|ts|tsx)`,
-  ],
-  addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: { // disable the addons
-        actions: false,
-        backgrounds: false, 
-        highlight: false, 
-        toolbars: false
-      },
-    },
-    "@storybook/addon-storysource"
-  ],
-  core: {
-    disableTelemetry: true,
-  },
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  typescript: {
-    check: false,
-    reactDocgen: false // Speed up as we put all props in argTypes
-  },
-  // Expose package versions and icons
-  previewHead: (head) =>
-    `${head}<script>
+	stories: [
+		`../${FOLDERS}/**/${IGNORE}.mdx`,
+		`../${FOLDERS}/**/${IGNORE}.stories.@(js|jsx|mjs|ts|tsx)`,
+	],
+	addons: [
+		{
+			name: "@storybook/addon-essentials",
+			options: {
+				// disable the addons
+				actions: false,
+				backgrounds: false,
+				highlight: false,
+				toolbars: false,
+			},
+		},
+		"@storybook/addon-storysource",
+	],
+	core: {
+		disableTelemetry: true,
+	},
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	typescript: {
+		check: false,
+		reactDocgen: false, // Speed up as we put all props in argTypes
+	},
+	// Expose package versions and icons
+	previewHead: (head) =>
+		`${head}<script>
       window.VERSION = ${JSON.stringify(version)};
       window.SVGS = ${JSON.stringify(
-        fg.sync(path.join(PUBLIC_DIR, '**', '*.svg')).map((file) => [
-          file.slice(PUBLIC_DIR.length),
-          fs.readFileSync(file, { encoding: 'utf-8' })
-        ])
-      )};
+				fg.sync(path.join(PUBLIC_DIR, "**", "*.svg")).map((file) => {
+					return {
+						file: path.relative(PUBLIC_DIR, file),
+						categories: illustrations[path.basename(file)] ?? [],
+						svg: fs.readFileSync(file, { encoding: "utf-8" }),
+					};
+				}),
+			)};
     </script>`,
 } satisfies StorybookConfig;
