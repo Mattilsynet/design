d970818285478a9e58ad2d53b796acb070b1cae2
eirikbacker <eirik.backer@mattilsynet.no>
2025-01-13 11:10:06 +0100

    docs: fix table story

--- designsystem/card/card.stories.tsx
+++ designsystem/card/card.stories.tsx
@@ -10,7 +10,10 @@ type Story = StoryObj<typeof meta>;
 
 const decorators: Story["decorators"] = [
 	(Story) => (
-		<div className={styles.grid}>
+		<div
+			className={styles.grid}
+			style={{ background: "var(--mt-gaasunge)", padding: "2em" }}
+		>
 			<Story />
 		</div>
 	),
@@ -19,4 +22,23 @@ const decorators: Story["decorators"] = [
 export const Default: Story = {
 	decorators,
 	render: () => <div className={styles.card}>Hei</div>,
+};
+
+export const Tableish: Story = {
+	decorators,
+	render: () => (
+		<div className={styles.card}>
+			<h2>PSC8CHE2023029471IN</h2>
+			<div>
+				<div>06.12.2024</div>
+				<div>19:37</div>
+			</div>
+			<div>
+				<div>GREENK</div>
+				<div>Frankrike</div>
+			</div>
+			<div>Bama Gruppen AS, avd. Trading</div>
+			<div>Daucus carota, Hydrangea paniculata, +4</div>
+		</div>
+	),
 };
