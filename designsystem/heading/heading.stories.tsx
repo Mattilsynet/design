bd93d33c6fb7e33c634ec72ab487a0ab8a6e8065
Marius Sørensen Dreyer <marius.s.dreyer@gmail.com>
2025-01-20 15:46:17 +0100

    feat: add heading styles

---
+++ designsystem/heading/heading.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from "@storybook/react";
+import styles from "../styles.module.css";
+
+const meta = {
+  title: "Designsystem/Heading",
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const decorators: Story["decorators"] = [
+  (Story) => (
+    <div className={styles.grid}>
+      <Story />
+    </div>
+  ),
+];
+
+export const Default: Story = {
+  decorators,
+  render: () => (
+    <>
+      <h2 className={styles.heading} data-size="2xl">
+        Heading data-size="2xl"
+      </h2>
+      <h2 className={styles.heading} data-size="xl">
+        Heading data-size="xl"
+      </h2>
+      <h2 className={styles.heading} data-size="lg">
+        Heading data-size="lg"
+      </h2>
+      <h2 className={styles.heading} data-size="md">
+        Heading data-size="md"
+      </h2>
+      <h2 className={styles.heading} data-size="sm">
+        Heading data-size="sm"
+      </h2>
+      <h2 className={styles.heading} data-size="xs">
+        Heading data-size="xs"
+      </h2>
+      <h2 className={styles.heading} data-size="2xs">
+        Heading data-size="2xs"
+      </h2>
+    </>
+  ),
+};
