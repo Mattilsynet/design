92b41302461acbeb914e769b44b0943c603e13ef
Marius Sørensen Dreyer <mariussd@users.noreply.github.com>
2025-01-22 14:56:38 +0100

    ci: add steps for clojure setup and publishing (#107)
    * ci: add steps for clojure setup and publishing
    
    * ci: separate workflows
    
    * chore: trigger workflow on feature branch for testing
    
    * chore: trigger workflow on workflow run
    
    * chore: another attempt at running this
    
    * chore: dry run of clojure publish
    
    * chore: dry run of clojure publish vol 2
    
    * chore: dry run of clojure publish vol 3
    
    * chore: release Clojure library 0.2.8-test.23
    
    * chore: bump java version and clojure setup version
    
    * chore: run npm run build
    
    * fix: attempt to create a new version vol 2
    
    * fix: set git config
    
    * chore: release Clojure library 0.2.8.23
    
    * fix: remove dryrun true if that has something to do with it
    
    * chore: release Clojure library 0.2.8.24
    
    * fix: switch to clojars token
    
    * chore: release Clojure library 0.2.8.25
    
    * fix: revert to clojars password
    
    * chore: release Clojure library 0.2.8.26
    
    * fix: try to use cli params
    
    * chore: release Clojure library 0.2.8.27
    
    * fix: switched up parameters
    
    * chore: release Clojure library 0.2.8.28
    
    * chore: release Clojure library 0.2.8.29
    
    * chore: release Clojure library 0.2.8.30
    
    * chore: release Clojure library 0.2.8.31
    
    * chore: release Clojure library 0.2.8.32
    
    * chore: release Clojure library 0.2.8.33
    
    * chore: release Clojure library 0.2.8.34
    
    * chore: release Clojure library 0.2.8.35
    
    * fix: debug environment variables
    
    * chore: release Clojure library 0.2.8.36
    
    * fix: try with .m2 and settings.xml
    
    * chore: release Clojure library 0.2.8.37
    
    * chore: remove debugging step
    
    * chore: release Clojure library 0.2.9.38
    
    * chore: revert testing changes
    
    * chore: remove double npm run build
    
    * chore: remove username and password from shell script
    
    * chore: cleanup
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- clojure/publish.sh
+++ clojure/publish.sh
@@ -14,4 +14,8 @@ git push
 git push --tags
 
 clojure -M:dev:jar 2> /dev/null
-mvn deploy:deploy-file -Dfile=mattilsynet-design.jar -DrepositoryId=clojars -Durl=https://clojars.org/repo -DpomFile=pom.xml
+mvn deploy:deploy-file \
+  -Dfile=mattilsynet-design.jar \
+  -DrepositoryId=clojars \
+  -Durl=https://clojars.org/repo \
+  -DpomFile=pom.xml
