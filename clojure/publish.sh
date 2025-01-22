90c9fcbd22a9465bd5b23f419313de15fade56f3
Marius Sørensen Dreyer <marius.s.dreyer@gmail.com>
2025-01-22 10:18:48 +0100

    chore: dry run of clojure publish vol 2

--- clojure/publish.sh
+++ clojure/publish.sh
@@ -14,4 +14,4 @@ git push
 git push --tags
 
 clojure -M:dev:jar 2> /dev/null
-mvn deploy:deploy-file -Dfile=mattilsynet-design.jar -DrepositoryId=clojars -Durl=https://clojars.org/repo -DpomFile=pom.xml
+mvn deploy:deploy-file -DdryRun=true -Dfile=mattilsynet-design.jar -DrepositoryId=clojars -Durl=https://clojars.org/repo -DpomFile=pom.xml
