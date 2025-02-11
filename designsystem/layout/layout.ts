6f2e6168e7ce3b196df963706412e412a32c9f55
Eirik Backer <eirik.backer@mattilsynet.no>
2025-02-11 12:51:19 +0100

    chore: update version to 0.3.2

--- designsystem/layout/layout.ts
+++ designsystem/layout/layout.ts
@@ -1,17 +1,23 @@
+import styles from '../styles.module.css';
 import { QUICK_EVENT, off, on } from '../utils';
 
-const handleToggleClick = ({ target }: Event) => {
-  // const el =
-  //   (target as Element)?.closest("nav") ||
-  //   document.querySelector("aside"); // "nav,aside,header,footer"
-  // const toggle = () =>
-  //   el?.setAttribute(
-  //     "data-expanded",
-  //     `${el?.getAttribute("data-expanded") === "false"}`,
-  //   );
+const CSS_APP = styles.app.split(' ')[0];
+const CSS_TOGGLE = `.${CSS_APP} > header + :not(main) > button:empty:first-child`;
 
-  // if (!document.startViewTransition) toggle();
-  // else document.startViewTransition(() => toggle());
+const handleToggleClick = ({ target: el }: Event) => {
+  const btn = (el as Element)?.nodeName === 'BUTTON' && (el as Element)?.closest(CSS_TOGGLE);
+
+  if (!btn) return;
+
+  const nav = btn.closest(`.${CSS_APP} > *`);
+  const toggle = () =>
+    nav?.setAttribute(
+      "data-expanded",
+      `${nav?.getAttribute("data-expanded") === "false"}`,
+    );
+
+  if (!document.startViewTransition) toggle();
+  else document.startViewTransition(() => toggle());
 };
 
 export function observe(el: Element) {
