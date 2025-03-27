81f0d40091c417ece5fb106dfc832137523db62e
eirikbacker <eirik.backer@mattilsynet.no>
2025-03-27 16:59:12 +0100

    chore: typo

--- designsystem/layout/app-observer.ts
+++ designsystem/layout/app-observer.ts
@@ -6,21 +6,21 @@ const KEY_TOGGLE = 'mtds-app-menu';
 
 const handleToggleClick = ({ target: el }: Event) => {
   if ((el as Element)?.closest('[data-command="toggle-app-expanded"]')) {
-    if (!document.startViewTransition) setExpaned(!getExpanded());
-    else document.startViewTransition(() => setExpaned(!getExpanded()));
+    if (!document.startViewTransition) setExpanded(!getExpanded());
+    else document.startViewTransition(() => setExpanded(!getExpanded()));
   }
 };
 
 const getExpanded = () =>
   !document.documentElement.style.getPropertyValue(CSS_TOGGLE)?.includes('--false');
 
-const setExpaned = (state: boolean) => {
+const setExpanded = (state: boolean) => {
   document.documentElement.style.setProperty(CSS_TOGGLE, `var(${CSS_TOGGLE}--${state})`);
   window.localStorage.setItem(KEY_TOGGLE, `${state}`);
 };
 
 if (IS_BROWSER) {
   const store = window.localStorage.getItem(KEY_TOGGLE);
-  setExpaned(store ? store === 'true' : getExpanded());
+  setExpanded(store ? store === 'true' : getExpanded());
   on(document, "click", handleToggleClick, QUICK_EVENT);
 }
