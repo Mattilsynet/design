2975cb779c956a7d05e04fa52294d8c064d4e286
Christian Johansen <christian.johansen@mattilsynet.no>
2025-04-11 13:51:26 +0200

    Don't commit changes when publishing the Clojure artifact (#244)
    * Don't commit changes when publishing the Clojure artifact
    
    * Don't keep a random version in checked-in pom.xml

--- clojure/publish.sh
+++ clojure/publish.sh
@@ -5,11 +5,7 @@ set -xe
 ./build.sh
 
 rm -f mattilsynet-design.jar
-version=$(clojure -A:dev -T:build bump-version)
-
-git add .
-git commit -m "chore: release Clojure library $version"
-git tag -a v$version -m "chore: tag Clojure library $version"
+clojure -A:dev -T:build bump-version
 
 clojure -M:dev:jar 2> /dev/null
 mvn deploy:deploy-file \
