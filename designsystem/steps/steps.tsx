a18af514448b94232ceabc4140517748fbcab4a1
eirikbacker <eirik.backer@mattilsynet.no>
2025-06-13 10:26:34 +0200

    fix(steps): always render as ol

--- designsystem/steps/steps.tsx
+++ designsystem/steps/steps.tsx
@@ -1,28 +1,14 @@
 import clsx from "clsx";
-import { type JSX, forwardRef } from "react";
-import type {
-	PolymorphicComponentPropWithRef,
-	PolymorphicRef,
-} from "../react-types";
+import { forwardRef } from "react";
 import styles from "../styles.module.css";
 
-export type StepsProps<As extends React.ElementType = "div"> =
-	PolymorphicComponentPropWithRef<
-		As,
-		{
-			"data-direction"?: "right" | "up" | "down";
-			"data-fade"?: "true" | "false" | "start" | "end";
-		}
-	>;
-
-type StepsComponent = <As extends React.ElementType = "div">(
-	props: StepsProps<As>,
-) => JSX.Element;
-
-export const Steps: StepsComponent = forwardRef<null>(function Steps<
-	As extends React.ElementType = "div",
->({ as, className, ...rest }: StepsProps<As>, ref?: PolymorphicRef<As>) {
-	const Tag = as || "div";
-
-	return <Tag className={clsx(styles.steps, className)} ref={ref} {...rest} />;
-}) as StepsComponent; // Needed to tell Typescript this does not return ReactNode but acutally JSX.Element
+export type StepsProps = React.ComponentPropsWithoutRef<"ol"> & {
+	"data-direction"?: "right" | "up" | "down";
+	"data-fade"?: "true" | "false" | "start" | "end";
+};
+export const Steps = forwardRef<HTMLOListElement, StepsProps>(function Steps(
+	{ className, ...rest }: StepsProps,
+	ref,
+) {
+	return <ol className={clsx(styles.steps, className)} ref={ref} {...rest} />;
+});
