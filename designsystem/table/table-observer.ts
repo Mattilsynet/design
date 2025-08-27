43ab67e1a9c8f7d6a4904fb57f0f09e5932558d8
eirikbacker <eirik.backer@mattilsynet.no>
2025-08-27 10:53:30 +0200

    feat: add info variant circle, adjust color details, simplify table aria

--- designsystem/table/table-observer.ts
+++ designsystem/table/table-observer.ts
@@ -5,28 +5,14 @@ const CSS_TABLE = styles.table.split(" ")[0];
 
 function handleTableMutation(tables: HTMLCollectionOf<HTMLTableElement>) {
 	for (const table of tables) {
-		const ths: string[] = []; // Add data-th="" to each cell to support data-mobile designs
+		const ths = Array.from(table.tHead?.rows[0]?.cells || [], (th) =>
+			th.innerText.trim(),
+		);
 
-		attr(table, "role", "table"); // Add helping role="" to ensure screen readers understand the table regardless of CSS display
-		if (table.caption) attr(table.caption, "role", "caption");
-		for (const group of [table.tHead, table.tFoot, ...table.tBodies])
-			if (group) {
-				attr(group, "role", "rowgroup");
-				const isTbody = group.nodeName === "TBODY";
-
-				for (const row of group.rows) {
-					attr(row, "role", "row");
-					for (const cell of row.cells) {
-						if (isTbody) attr(cell, "data-th", ths[cell.cellIndex] || ":empty");
-						else ths.push(cell.innerText.trim()); // Using innerText to only include visible text
-						if (cell.nodeName === "TD") attr(cell, "role", "cell");
-						else {
-							attr(cell, "role", isTbody ? "rowheader" : "columnheader");
-							attr(cell, "scope", isTbody ? "row" : "col");
-						}
-					}
-				}
-			}
+		for (const tbody of table.tBodies)
+			for (const row of tbody.rows)
+				for (const cell of row.cells)
+					attr(cell, "data-th", ths[cell.cellIndex] || ":empty");
 	}
 }
 
