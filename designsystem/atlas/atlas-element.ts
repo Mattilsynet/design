c14995878377317eb888dce6a03aa3d964a527eb
eirikbacker <eirik.backer@mattilsynet.no>
2025-11-12 08:30:55 +0100

    feat(Atlas): now supports popover

--- designsystem/atlas/atlas-element.ts
+++ designsystem/atlas/atlas-element.ts
@@ -1,11 +1,20 @@
-import L from "leaflet";
+import L, { type PointExpression } from "leaflet";
 import LeafletCSS from "leaflet/dist/leaflet.css?raw";
 import type {} from "leaflet.markercluster";
 import type * as ReactTypes from "react";
-import { attr, debounce, IS_BROWSER, MTDSElement, tag } from "../utils";
 import css from "./atlas.css?raw";
 import "./cluster.js";
+import {
+	attr,
+	debounce,
+	IS_BROWSER,
+	MTDSElement,
+	off,
+	on,
+	tag,
+} from "../utils";
 export { L };
+
 declare global {
 	namespace React.JSX {
 		interface IntrinsicElements {
@@ -20,14 +29,13 @@ declare global {
 	}
 }
 
-// const EVENTS = "popupopen popupclose move zoom resize";
+const MARKER_SIZE = [30, 30];
 const BOUNDS_NORWAY: L.LatLngBoundsLiteral = [
 	[57.5, 4.73],
 	[71.5, 31.44],
 ];
 
 // Turns out Kartverket's color tiles are better in grayscale than their grayscale tiles
-// const TILES_GRAY = "https://cache.kartverket.no/v1/wmts/1.0.0/topograatone/default/webmercator/{z}/{y}/{x}.png";
 const TILES_COLOR =
 	"https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png";
 
@@ -74,7 +82,7 @@ export class MTDSAtlasElement extends MTDSElement {
 
 		this.map.addControl(new L.Control.Attribution({ prefix: "" }));
 		this.map.addControl(new L.Control.Zoom({ position: "bottomright" }));
-		// this.map.on(EVENTS, handleEvents);
+		this.map.on("popupopen", handlePopupById);
 
 		// Initial setup when map is connected
 		MTDSAtlasElement.observedAttributes.forEach((name) => {
@@ -108,7 +116,6 @@ export class MTDSAtlasElement extends MTDSElement {
 	}
 	disconnectedCallback() {
 		this.map?.remove();
-		// this.map?.off(EVENTS, handleEvents);
 	}
 }
 
@@ -116,7 +123,7 @@ export class MTDSAtlasElement extends MTDSElement {
 if (IS_BROWSER && !window.customElements.get("mtds-atlas")) {
 	L.Marker.prototype.options.icon = new L.DivIcon({
 		html: '<svg class="map-pin" viewBox="0 0 256 256"><rect x="78" y="50" width="100" height="100" /><path fill="var(--mtds-)" d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"/></svg>',
-		iconSize: [30, 30],
+		iconSize: MARKER_SIZE as PointExpression,
 		iconAnchor: [15, 30],
 		popupAnchor: [0, -15],
 	});
@@ -124,7 +131,6 @@ if (IS_BROWSER && !window.customElements.get("mtds-atlas")) {
 }
 
 // TODO: Style markers
-// TODO: Popover
 // TODO: Synlig label til marker
 
 const fitWhenMarkers = debounce(function (this: MTDSAtlasElement) {
@@ -132,59 +138,44 @@ const fitWhenMarkers = debounce(function (this: MTDSAtlasElement) {
 	if (this.cluster?.getBounds().isValid()) this.setView("fit");
 }, 100);
 
-// const handleEvents: L.LeafletEventHandlerFn = ({ type, target, popup }) => {
-// 	const self = target.getContainer();
+const HANDLE_POPUP_EVENTS = "popupclose move resize zoomanim viewreset";
+const handlePopupById = ({ target: map, popup }: L.LeafletEvent) => {
+	const id = popup.getContent();
+	const popover = String(id)[0] === "#" && document.getElementById(id.slice(1));
 
-// 	if (type === "popupopen" || type === "popupclose") {
-// 		if (self._popup) {
-// 			const [el] = self._popup;
-// 			self._popup = el.hidePopover();
-// 			self._popup = undefined;
-// 		}
-// 		const id = type === "popupopen" && popup.getContent();
-// 		const el = String(id)[0] === "#" && document.getElementById(id.slice(1));
+	if (!!popover && popover.closest("mtds-atlas")) {
+		const latlng = popup.getLatLng();
+		const mode = attr(popover, "popover"); // Save current popover mode
+		attr(popup.getElement(), "hidden", ""); // Hide Leaflet popup element
+		attr(popover, "data-popover", "open"); // Show popover
+		attr(popover, "popover", "manual"); // Disable Popover API to show and ensure overflow: clip on mtds-atlas works
 
-// 		if (el) {
-// 			attr(el, "popover", "manual");
-// 			attr(popup.getElement(), "data-atlas-popup", id);
-// 			el.showPopover();
-// 			self._popup = [el, popup];
-// 			anchorPopover(target, popup, el);
-// 		}
-// 	} else if (self._popup) {
-// 		anchorPopover(target, self._popup[1], self._popup[0]);
-// 	}
-// };
+		const resize = new ResizeObserver(() => update());
+		const update = (event?: L.LeafletEvent) => {
+			if (event?.type === "popupclose" && event?.popup === popup) {
+				resize.disconnect();
+				map.off(HANDLE_POPUP_EVENTS, update);
+				attr(popup.getElement(), "hidden", null); // Un-hide Leaflet popup element
+				attr(popover, "data-popover", null); // Hide popover
+				attr(popover, "popover", mode); // Re-enable Popover API
+				off(popover, "click", click);
+			} else {
+				const point = map.latLngToContainerPoint(latlng);
+				const x = point.x - popover.offsetWidth / 2 - MARKER_SIZE[0] / 2;
+				const y = point.y - popover.offsetHeight - MARKER_SIZE[1];
+				popover.style.translate = `${x}px ${y}px`;
+			}
+		};
+		const click = ({ target: el }: Event) =>
+			(el as Element)?.closest?.('[popovertargetaction="hide"]') &&
+			popup.close();
 
-// const anchorPopover = (map: L.Map, popup: L.Popup, popover: HTMLElement) => {
-// 	const point = map.latLngToContainerPoint(popup.getLatLng() as L.LatLng);
-// 	const rect = map.getContainer().getBoundingClientRect();
-// 	popover.style.translate = `${rect.x + point.x + window.scrollX}px ${rect.y + point.y + window.scrollY}px`;
-// };
-
-// .on("click", ({ target: marker, originalEvent: event }) => {
-// 	const id = (marker.options as Record<string, string>)?.popoverTarget;
-// 	const el = document.getElementById(id || "");
-
-// 	if (el) {
-// 		console.log(el, this);
-// 		event.stopPropagation();
-// 		el.showPopover();
-// 		this.popup = el;
-// 		this.map?.fire("move"); // Trigger position update
-
-// 		// 	const updatePosition = () => {
-// 		// 		const point = this.map?.latLngToContainerPoint(marker.getLatLng());
-// 		// 		const rect = this.getBoundingClientRect();
-// 		// 		console.dir(rect);
-// 		// 		// console.log(point, this.map?.getBounds().getCenter().lat);
-// 		// 		if (point)
-// 		// 			el.style.translate = `${rect.x + point.x + window.scrollX}px ${rect.y + point.y + window.scrollY}px`;
-// 		// 	};
-
-// 		// 	updatePosition();
-// 	}
-// })
+		map.on(HANDLE_POPUP_EVENTS, update);
+		on(popover, "click", click); // Listen for close actions inside popover since we are not using actual Popover API
+		resize.observe(popover); // Listen for changes of popover size
+		update(); // Initial position
+	}
+};
 
 // export type Adresse = {
 // 	adressekode: number;
