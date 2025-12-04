a1c0bce61a432d1a90cc04c3a544f02edce21994
eirikbacker <eirik.backer@mattilsynet.no>
2025-12-04 12:37:37 +0100

    chore: cleanup

--- designsystem/chart/chart.stories.tsx
+++ designsystem/chart/chart.stories.tsx
@@ -143,7 +143,7 @@ export const Column: Story = {
 
 export const ColumnSingleDataset: Story = {
 	render: () => (
-		<mtds-chart data-variant="column" style={{ marginBlock: 900 }}>
+		<mtds-chart data-variant="column">
 			<table>
 				<thead>
 					<tr>
