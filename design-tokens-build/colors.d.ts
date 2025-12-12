dd81ea441fb4db09989e8050b692e7a87865c371
eirikbacker <eirik.backer@mattilsynet.no>
2025-12-12 10:02:26 +0100

    fix: generate new surface-tokens

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -5,6 +5,7 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
+    main: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
--- design-tokens-build/types.d.ts
+++ design-tokens-build/types.d.ts
@@ -4,6 +4,7 @@ import type {} from '@digdir/designsystemet-types';
 // Augment types based on theme
 declare module '@digdir/designsystemet-types' {
   export interface ColorDefinitions {
+    main: never;
     neutral: never;
   }
   export interface SeverityColorDefinitions {
