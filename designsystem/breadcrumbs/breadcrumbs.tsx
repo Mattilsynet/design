c88d9aab4383fca9091f386b70bf486d7a254564
eirikbacker <eirik.backer@mattilsynet.no>
2026-01-28 13:50:42 +0100

    docs: add details to migration

--- designsystem/breadcrumbs/breadcrumbs.tsx
+++ designsystem/breadcrumbs/breadcrumbs.tsx
@@ -4,7 +4,12 @@ import type { CustomReactElementProps } from "../react-types";
 import styles from "../styles.module.css";
 import { toCustomElementProps } from "../utils";
 
-export type BreadcrumbsProps = CustomReactElementProps<DSBreadcrumbsElement>;
+export type BreadcrumbsProps = CustomReactElementProps<DSBreadcrumbsElement> & {
+	/**
+	 * @deprecated `as` prop is no longer supported.
+	 */
+	as?: never; // Remove `as` prop support
+};
 
 export const Breadcrumbs = forwardRef<DSBreadcrumbsElement, BreadcrumbsProps>(
 	function Breadcrumbs(rest, ref) {
