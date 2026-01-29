50b4ee92907517b606fcdd1c1d98acafc25f6750
eirikbacker <eirik.backer@mattilsynet.no>
2026-01-29 08:37:51 +0100

    fix: use settooltipelement function

--- designsystem/tooltip/tooltip-element.ts
+++ designsystem/tooltip/tooltip-element.ts
@@ -1,16 +1,10 @@
+import { setTooltipElement } from "@digdir/designsystemet-web";
 import styles from "../styles.module.css";
-import { isBrowser, onHotReload, onMutation } from "../utils";
+import { isBrowser, tag } from "../utils";
 
-// Set custom tooltip element, will soon be replaced by `setTooltipElement` when exposed by @digdir/designsystemet-web
-const TIP = isBrowser() ? document.getElementsByClassName("ds-tooltip") : [];
-const CSS_TIP = styles._tooltip.split(" ");
-const addCSSClassToTooltips = () => {
-	for (const tip of TIP) tip.classList.add(...CSS_TIP);
-};
-
-onHotReload("tooltip", () => [
-	onMutation(document, addCSSClassToTooltips, {
-		childList: true,
-		subtree: true,
-	}),
-]);
+// Set custom tooltip element for designsystemet-web
+if (isBrowser())
+	setTooltipElement(
+		document.getElementById("mtds-tooltip") ||
+			tag("div", { class: styles._tooltip, id: "mtds-tooltip" }),
+	);
